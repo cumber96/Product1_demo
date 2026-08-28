79ace7dca604e06c67f98c92080de3e3fa04d8b1
cumber96 <nowgnoheel@gmail.com>
2026-08-28 07:24:58 +0000

    템플릿용 설정 및 스타일 파일 분리

---
+++ config.js
@@ -0,0 +1,8 @@
+window.PRODUCT_CONFIG = {
+  pageTitle: "차주 연락",
+  plateNumber: "123가 5678",
+  phoneNumber: "01012345678",
+  messageTitle: "차량 이동이 필요하신가요?",
+  callButtonText: "전화 걸기",
+  footerText: "연결 시 통신사 통화료가 발생할 수 있습니다"
+};
